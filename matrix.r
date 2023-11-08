e84ff13da92f706ba3ba07238dd70aa5632da487
nicorobin0508 <117674705+nicorobin0508@users.noreply.github.com>
2023-11-08 23:29:28 +0530

    Create matrix.r

---
+++ matrix.r
@@ -0,0 +1,41 @@
+> ml<-matrix (data=1:4, nrow-2, ncol=2)
+> print (ml)
+[1] [2]
+3
+4
+[1,1
+[2,]
+2
+> m2<-matrix (data=1:4, nrow-2, ncol=2)
+>print (m2)
+[1,1
+[2,1
+>#addition
+>result<-ml-m2
+>print (result)
+[1] [2]
+[1,1
+[2,]
+[1] [2]
+1
+3
+2
+4
+2
+4
+>substraction
+[1,1
+[2,]
+Error: object 'substraction' not found
+> #subtraction
+>result<-ml-m2
+> print (result)
+[1] [2]
+H
+6
+B
+O
+O
+0
+10
+> #miltiplication
