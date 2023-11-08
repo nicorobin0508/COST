eb965697ce1d80a7e24977519933c9b3a823f328
nicorobin0508 <117674705+nicorobin0508@users.noreply.github.com>
2023-11-08 23:26:06 +0530

    Create BC.r

---
+++ BC.r
@@ -0,0 +1,25 @@
+> s<-"hello world"
+> print (s)
+[1] "hello world"
+> # numeric
+> x <- 10.5
+> class (x)
+[1] "numeric"
+> # integer
+> X <- 1000L
+> class (x)
+[1] "integer"
+UNI
+> # complex
+>z<-91 + 3
+> class (x)
+[1] "complex"
+# character/string
+x <- "R is exciting"
+> class (X)
+[1] "character"
+VN
+> #logical/boolean
+X <- TRUE
+class (X)
+[1] "logical"
