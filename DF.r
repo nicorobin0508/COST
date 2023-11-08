6f7783aac4a116c7010be491fdcd5ea73c6a0370
nicorobin0508 <117674705+nicorobin0508@users.noreply.github.com>
2023-11-08 23:27:25 +0530

    Create DF.r

---
+++ DF.r
@@ -0,0 +1,13 @@
+6. Data Frame
+A data frame is used for storing data tables. It is a list of vectors of equal length.
+> name <- c("Yashashree",
+"Vedshree")
+> age <-c(11,7)
+> class <-c("6th", "1st")
+> df = data.frame (name, age, class)
+> df
+name age class
+1 Yashashree 11 6th
+7 1st
+2
+Vedshree
